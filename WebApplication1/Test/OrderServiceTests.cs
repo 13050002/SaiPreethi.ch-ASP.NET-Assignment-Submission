11ab165899472d912426ba9673d1a44aef85ca51
Sai Preethi <preethisai75@gmail.com>
2025-03-16 02:34:29 -0700

    Added CI/CD pipeline

---
+++ WebApplication1/Test/OrderServiceTests.cs
@@ -0,0 +1,28 @@
+using Xunit;
+//using OrderProcessingSystem.Models;
+//using OrderProcessingSystem.Services;
+
+public class OrderServiceTests
+{
+    private readonly OrderService _service = new OrderService();
+
+    [Fact]
+    public void Discount_Applied_For_Loyal_Customer()
+    {
+        var order = new Order { OrderAmount = 100, CustomerType = "Loyal" };
+        var result = _service.CalculateDiscount(order);
+
+        Assert.Equal(10, result.Discount);
+        Assert.Equal(90, result.FinalTotal);
+    }
+
+    [Fact]
+    public void No_Discount_For_New_Customer()
+    {
+        var order = new Order { OrderAmount = 100, CustomerType = "New" };
+        var result = _service.CalculateDiscount(order);
+
+        Assert.Equal(0, result.Discount);
+        Assert.Equal(100, result.FinalTotal);
+    }
+}
